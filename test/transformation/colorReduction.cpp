d412082e9951e93b3847260691e7f9b96368969d
Thomas Schlott <thomas.schlott@outlook.de>
2022-12-25 17:36:34 +0100

    colorReduction: Started implementation of the color reduction transformer

---
+++ test/transformation/colorReduction.cpp
@@ -0,0 +1,13 @@
+#include "THzImage/transformation/colorReduction.hpp"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace Terrahertz::UnitTests {
+
+struct Transformation_ColorReduction : public testing::Test
+{};
+
+TEST_F(Transformation_ColorReduction, ConstructionCorrectWhenUsingBaseTransformer) {}
+
+} // namespace Terrahertz::UnitTests
