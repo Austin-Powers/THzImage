743bdcdfb8b3d4874a529a0285744934fff74f6c
Thomas Schlott <thomas.schlott@outlook.de>
2023-01-04 14:08:15 +0100

    added test to notice changes in the reduction quality

--- test/io/gifWriter.cpp
+++ test/io/gifWriter.cpp
@@ -1,5 +1,8 @@
 #include "THzImage/io/gifWriter.hpp"
 
+#include "THzImage/common/image.hpp"
+#include "THzImage/io/testImageGenerator.hpp"
+
 #include <gtest/gtest.h>
 
 namespace Terrahertz::UnitTests {
@@ -20,6 +23,25 @@ TEST_F(IO_GIFWriter_ColorReduction, InitialStateCorrect)
         BGRAPixel pixel{static_cast<std::uint8_t>(i), static_cast<std::uint8_t>(i), static_cast<std::uint8_t>(i)};
         EXPECT_EQ(sut.convert(pixel), 0U);
     }
+}
+
+TEST_F(IO_GIFWriter_ColorReduction, ConversionResultCloseToOriginal)
+{
+    BGRAImage          image{};
+    TestImageGenerator generator{Rectangle{64U, 64U}};
+    EXPECT_TRUE(image.read(&generator));
+    sut.analyze(gsl::span<BGRAPixel const>{&image[0U], image.dimensions().area()});
+    auto const colorTable = sut.colorTable();
+
+    auto accumulatedDeviations = 0.0;
+    for (auto const i : image.dimensions().range())
+    {
+        auto const oColor = image[i];
+        auto const rColor = colorTable[sut.convert(oColor)];
+        accumulatedDeviations += oColor.distanceSquared(rColor);
+    }
+    accumulatedDeviations /= image.dimensions().area();
+    EXPECT_NEAR(accumulatedDeviations, 268.63, 0.05);
 }
 
 } // namespace Terrahertz::UnitTests
