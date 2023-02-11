e9caa89b54373681e12bc528727b2b47c1115471
Thomas Schlott <thomas.schlott@outlook.de>
2023-02-11 18:48:12 +0100

    Implemented TimedReader to bind the reading from another reader to certain time slots

---
+++ include/THzImage/io/timedReader.hpp
@@ -0,0 +1,138 @@
+#ifndef THZ_IMAGE_IO_TIMEDREADER_HPP
+#define THZ_IMAGE_IO_TIMEDREADER_HPP
+
+#include "THzImage/common/iImageReader.hpp"
+
+#include <chrono>
+#include <condition_variable>
+#include <cstdint>
+#include <type_traits>
+
+namespace Terrahertz {
+
+/// @brief Wraps another reader class to add the ability to let time pass between reading cycles.
+///
+/// @tparam TWrapped The reader class to wrap.
+template <ImageReader TWrapped>
+class TimedReader : public IImageReader<typename TWrapped::PixelType>
+{
+public:
+    /// @brief The type of clock used by this class for time keeping.
+    using Clock = std::chrono::steady_clock;
+
+    /// @brief The type of time point used by this class for time keeping.
+    using TimePoint = std::chrono::time_point<Clock>;
+
+    /// @brief Structure containing information about the operation of the wrapper.
+    struct Info
+    {
+        /// @brief The amount of time slots that passed without reading.
+        std::uint32_t missedTimeSlots{};
+
+        /// @brief The total amount of time slots that passed since setting the interval.
+        std::uint32_t totalTimeSlots{};
+    };
+
+    /// @brief Initializes a new TimedReader using the given parameters.
+    ///
+    /// @tparam ...TParamTypes The types of the parameters for the constructor of the wrapped reader.
+    /// @param pInterval The interval in which the reader cycle can be called.
+    /// @param ...params The parameter for the constructor of the wrapped reader.
+    /// @remark The timing works by delaying the init() call till the next time-point is reached.
+    ///         Time-points are bound to the given interval.
+    template <typename... TParamTypes>
+    TimedReader(std::chrono::milliseconds const pInterval,
+                TParamTypes &&...params) noexcept(std::is_nothrow_constructible_v<TWrapped, TParamTypes...>)
+        : _wrapped{std::forward<TParamTypes>(params)...}
+    {
+        if (!setInterval(pInterval))
+        {
+            // will always return true
+            (void)setInterval(std::chrono::milliseconds{1000});
+        }
+    }
+
+    /// @brief Returns a reference to the wrapped reader, to provide access to special methods.
+    ///
+    /// @return A reference to the wrapped reader.
+    TWrapped &wrappedReader() noexcept { return _wrapped; }
+
+    /// @brief Returns the current interval.
+    ///
+    /// @return The current interval.
+    std::chrono::milliseconds interval() const noexcept { return _interval; }
+
+    /// @brief Sets the interval in which the reader cycle can be called.
+    ///
+    /// @param pInterval The interval in which the reader cycle can be called.
+    /// @return True if the interval was updated, false otherwise (if pInterval is zero).
+    [[nodiscard]] bool setInterval(std::chrono::milliseconds const &pInterval) noexcept
+    {
+        if (pInterval.count() <= 0)
+        {
+            return false;
+        }
+        _interval  = pInterval;
+        _nextPoint = Clock::now() + _interval;
+        _info      = Info{};
+        return true;
+    }
+
+    /// @brief Returns inforamtion about the operation of the reader.
+    ///
+    /// @return Inforamtion about the operation of the reader.
+    Info info() const noexcept { return _info; }
+
+    /// @copydoc IImageReader::imagePresent
+    bool imagePresent() const noexcept override { return _wrapped.imagePresent(); }
+
+    /// @copydoc IImageReader::init
+    bool init() noexcept override
+    {
+        std::unique_lock<std::mutex> lock{_mutex};
+
+        auto const currentTime = Clock::now();
+        while ((_nextPoint - currentTime).count() <= 0U)
+        {
+            _nextPoint += _interval;
+            ++_info.missedTimeSlots;
+            ++_info.totalTimeSlots;
+        }
+        _cv.wait_until(lock, _nextPoint);
+        _nextPoint += _interval;
+        ++_info.totalTimeSlots;
+        return _wrapped.init();
+    }
+
+    /// @copydoc IImageReader::dimensions
+    Rectangle dimensions() const noexcept override { return _wrapped.dimensions(); }
+
+    /// @copydoc IImageReader::read
+    bool read(gsl::span<typename TWrapped::PixelType> buffer) noexcept override { return _wrapped.read(buffer); }
+
+    /// @copydoc IImageReader::deinit
+    void deinit() noexcept override { _wrapped.deinit(); }
+
+private:
+    /// @brief The next point in time the reader cycle can be called.
+    TimePoint _nextPoint{Clock::now()};
+
+    /// @brief The interval in which the reader can be called.
+    std::chrono::milliseconds _interval{};
+
+    /// @brief The mutex for the condition variable.
+    std::mutex _mutex{};
+
+    /// @brief The condition variable to call wait_until on.
+    std::condition_variable _cv{};
+
+    /// @brief The wrapped reader.
+    TWrapped _wrapped;
+
+    /// @brief The info about the operation of the reader.
+    Info _info{};
+};
+
+} // namespace Terrahertz
+
+#endif // !THZ_IMAGE_IO_TIMEDREADER_HPP
---
+++ test/io/timedReader.cpp
@@ -0,0 +1,123 @@
+#include "THzImage/io/timedReader.hpp"
+
+#include "THzImage/common/image.hpp"
+#include "THzImage/io/testImageGenerator.hpp"
+
+#include <chrono>
+#include <gtest/gtest.h>
+
+namespace Terrahertz::UnitTests {
+
+struct IO_TimedReader : public testing::Test
+{
+    using BaseReader = TestImageGenerator;
+    using TestReader = TimedReader<BaseReader>;
+    using millisecs  = std::chrono::milliseconds;
+};
+
+TEST_F(IO_TimedReader, NewlyConstructedInstanceCorrect)
+{
+    Rectangle const dimensions{10U, 10U};
+    millisecs const interval{10U};
+
+    BaseReader expectedInnerReader{dimensions};
+    TestReader sut{interval, dimensions};
+
+    EXPECT_EQ(expectedInnerReader.dimensions(), sut.wrappedReader().dimensions());
+    EXPECT_EQ(sut.interval(), interval);
+    EXPECT_EQ(sut.info().missedTimeSlots, 0U);
+    EXPECT_EQ(sut.info().totalTimeSlots, 0U);
+}
+
+TEST_F(IO_TimedReader, ConstructionWithZeroInterval)
+{
+    TestReader sut{millisecs{0U}, Rectangle{2U, 2U}};
+    EXPECT_EQ(sut.interval(), millisecs{1000U});
+}
+
+TEST_F(IO_TimedReader, UpdatingInterval)
+{
+    millisecs const interval{10U};
+
+    TestReader sut{interval, Rectangle{2U, 2U}};
+    EXPECT_FALSE(sut.setInterval(millisecs{0U}));
+    EXPECT_EQ(sut.interval(), interval);
+
+    millisecs newInterval{100U};
+    EXPECT_TRUE(sut.setInterval(newInterval));
+    EXPECT_EQ(sut.interval(), newInterval);
+    EXPECT_EQ(sut.info().missedTimeSlots, 0U);
+    EXPECT_EQ(sut.info().totalTimeSlots, 0U);
+}
+
+TEST_F(IO_TimedReader, DelayReadingAsExpected)
+{
+    millisecs const    interval{16U};
+    Rectangle const    dimensions{2U, 2U};
+    BGRAImage          referenceImage{};
+    TestImageGenerator generator{dimensions};
+    EXPECT_TRUE(referenceImage.read(&generator));
+
+    BGRAImage  image{};
+    TestReader sut{interval, dimensions};
+    auto const startTime = std::chrono::steady_clock::now();
+
+    auto const targetCount = 12LL;
+    auto       durations   = 0LL;
+    for (auto i = 1LL; i <= targetCount; ++i)
+    {
+        EXPECT_TRUE(image.read(&sut));
+        auto const readTime = std::chrono::steady_clock::now();
+        auto const duration = std::chrono::duration_cast<millisecs>(readTime - startTime);
+        durations += (duration.count() / i);
+
+        ASSERT_EQ(referenceImage.dimensions(), image.dimensions());
+        for (auto const idx : image.dimensions().range())
+        {
+            EXPECT_EQ(image[idx], referenceImage[idx]);
+        }
+    }
+    durations /= targetCount;
+    EXPECT_LE(durations, interval.count() + 4);
+    EXPECT_GE(durations, interval.count() - 4);
+    EXPECT_EQ(sut.info().missedTimeSlots, 0U);
+    EXPECT_EQ(sut.info().totalTimeSlots, targetCount);
+}
+
+TEST_F(IO_TimedReader, ReaderDelaysToFixTimePoints)
+{
+    millisecs const interval{20U};
+    millisecs const sleepInterval{interval.count() + 2U};
+    Rectangle const dimensions{2U, 2U};
+
+    BGRAImage  image{};
+    TestReader sut{interval, dimensions};
+    auto const startTime = std::chrono::steady_clock::now();
+
+    auto const targetCount = 12LL;
+    auto       durations   = 0LL;
+    for (auto i = 1LL; i <= targetCount; ++i)
+    {
+        std::this_thread::sleep_for(sleepInterval);
+        EXPECT_TRUE(image.read(&sut));
+        auto const readTime = std::chrono::steady_clock::now();
+        auto const duration = std::chrono::duration_cast<millisecs>(readTime - startTime);
+        durations += (duration.count() / i);
+    }
+    durations /= targetCount;
+    EXPECT_LE(durations, (interval.count() * 2) + 4);
+    EXPECT_GE(durations, (interval.count() * 2) - 4);
+    EXPECT_EQ(sut.info().missedTimeSlots, targetCount);
+    EXPECT_EQ(sut.info().totalTimeSlots, targetCount * 2U);
+
+    // as these test are rather time consuming, we will test here if setInterval resets the info
+    EXPECT_FALSE(sut.setInterval(millisecs{0U}));
+    EXPECT_EQ(sut.info().missedTimeSlots, targetCount);
+    EXPECT_EQ(sut.info().totalTimeSlots, targetCount * 2U);
+
+    EXPECT_TRUE(sut.setInterval(millisecs{20U}));
+    EXPECT_EQ(sut.info().missedTimeSlots, 0U);
+    EXPECT_EQ(sut.info().totalTimeSlots, 0U);
+}
+
+} // namespace Terrahertz::UnitTests
