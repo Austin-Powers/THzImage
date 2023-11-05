199ce290e65aa7d49aa45bbf3d1acc0d4185f275
Thomas Schlott <thomas.schlott@outlook.de>
2023-11-05 03:40:32 +0100

    bufferTransformer: added class for docking to a buffer

---
+++ include/THzImage/handling/bufferTransformer.hpp
@@ -0,0 +1,93 @@
+#ifndef THZ_IMAGE_HANDLING_BUFFERTRANSFORMER_HPP
+#define THZ_IMAGE_HANDLING_BUFFERTRANSFORMER_HPP
+
+#include "THzImage/common/iImageTransformer.hpp"
+#include "THzImage/handling/imageRingBuffer.hpp"
+
+#include <cstddef>
+
+namespace Terrahertz {
+
+/// @brief Transformer starting a chain of transformers from a imageRingBuffer.
+///
+/// @tparam TPixelType The type of pixel used by the buffer.
+template <typename TPixelType>
+class BufferTransformer : public IImageTransformer<TPixelType>
+{
+public:
+    /// @brief Default initializes a BufferTransformer.
+    BufferTransformer() noexcept = default;
+
+    /// @brief Updates the setup used by the BufferTransformer.
+    ///
+    /// @param buffer The buffer to get the images from.
+    /// @param slotIdx The index of the slot in the buffer.
+    /// @param callNext True if nextImage() should call next() on the buffer, false otherwise.
+    /// @return True if update was successful, false otherwise.
+    bool update(ImageRingBuffer<TPixelType> &buffer, size_t const slotIdx, bool callNext) noexcept
+    {
+        if (slotIdx < buffer.slots())
+        {
+            _buffer   = &buffer;
+            _slotIdx  = slotIdx;
+            _callNext = callNext;
+            updateView();
+            return true;
+        }
+        return false;
+    }
+
+    /// @copydoc IImageTransformer::dimensions
+    Rectangle dimensions() const noexcept override { return _view.dimensions(); }
+
+    /// @copydoc IImageTransformer::transform
+    bool transform(TPixelType &pixel) noexcept override { return _view.transform(pixel); }
+
+    /// @copydoc IImageTransformer::skip
+    bool skip() noexcept override { return _view.skip(); }
+
+    /// @copydoc IImageTransformer::reset
+    bool reset() noexcept override
+    {
+        if (_buffer != nullptr)
+        {
+            // this method could be called before update(...) was called, rendering _buffer == nullptr
+            updateView();
+        }
+        return true;
+    }
+
+    /// @copydoc IImageTransformer::nextImage
+    bool nextImage() noexcept override
+    {
+        if (_callNext)
+        {
+            if (_buffer->next())
+            {
+                updateView();
+                return true;
+            }
+        }
+        return false;
+    }
+
+private:
+    /// @brief Updates the view of the transformer.
+    void updateView() noexcept { _view = _buffer->operator[](_slotIdx).view(); }
+
+    /// @brief The buffer to get the image from.
+    ImageRingBuffer<TPixelType> *_buffer{};
+
+    /// @brief The index of the slot in the buffer.
+    size_t _slotIdx{};
+
+    /// @brief True if nextImage() should call next() on the buffer, false otherwise.
+    bool _callNext{};
+
+    /// @brief The view of the current image.
+    ImageView<TPixelType> _view{};
+};
+
+} // namespace Terrahertz
+
+#endif // !THZ_IMAGE_HANDLING_BUFFERTRANSFORMER_HPP
---
+++ test/handling/bufferTransformer.cpp
@@ -0,0 +1,64 @@
+#include "THzImage/handling/bufferTransformer.hpp"
+
+#include "THzImage/common/image.hpp"
+#include "THzImage/common/pixel.hpp"
+#include "THzImage/io/testImageGenerator.hpp"
+
+#include <cstdint>
+#include <gtest/gtest.h>
+
+namespace Terrahertz::UnitsTests {
+
+struct Handling_BufferTransformer : public testing::Test
+{
+    TestImageGenerator generator{Rectangle{4U, 4U}};
+
+    ImageRingBuffer<BGRAPixel> buffer{generator, 3U};
+
+    BufferTransformer<BGRAPixel> sut{};
+};
+
+TEST_F(Handling_BufferTransformer, DefaultConstructedBehavesLikeDefaultView)
+{
+    BGRAPixel pixel{};
+
+    ImageView<BGRAPixel> defaultView{};
+    EXPECT_EQ(sut.dimensions(), defaultView.dimensions());
+    EXPECT_EQ(sut.reset(), defaultView.reset());
+    EXPECT_EQ(sut.nextImage(), defaultView.nextImage());
+}
+
+TEST_F(Handling_BufferTransformer, UpdateRejectsInvalidValues)
+{
+    EXPECT_FALSE(sut.update(buffer, 3U, true));
+    EXPECT_FALSE(sut.update(buffer, 4U, false));
+}
+
+TEST_F(Handling_BufferTransformer, UpdateWithValidValuesInteractsWithGivenBuffer)
+{
+    EXPECT_TRUE(buffer.next());
+    EXPECT_EQ(buffer.count(), 1U);
+    EXPECT_TRUE(sut.update(buffer, 0U, false));
+    BGRAImage target{};
+    EXPECT_TRUE(target.storeResultOf(sut));
+
+    for (auto i = 0U; i < buffer[0U].dimensions().area(); ++i)
+    {
+        EXPECT_EQ(target[i], buffer[0U][i]);
+    }
+    EXPECT_EQ(buffer.count(), 1U);
+}
+
+TEST_F(Handling_BufferTransformer, NextImageCallingNextOnTheBuffer)
+{
+    EXPECT_TRUE(buffer.next());
+    EXPECT_EQ(buffer.count(), 1U);
+    EXPECT_TRUE(sut.update(buffer, 0U, false));
+    EXPECT_FALSE(sut.nextImage());
+    EXPECT_EQ(buffer.count(), 1U);
+    EXPECT_TRUE(sut.update(buffer, 0U, true));
+    EXPECT_TRUE(sut.nextImage());
+    EXPECT_EQ(buffer.count(), 2U);
+}
+
+} // namespace Terrahertz::UnitsTests
