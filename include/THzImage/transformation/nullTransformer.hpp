fded9d2718bb8f2e3e5101525056c5d3198a45e8
Thomas Schlott <thomas.schlott@outlook.de>
2023-12-30 02:39:58 +0100

    Added a null object for image transformers to enable easy creation of default constructors for other transformers

---
+++ include/THzImage/transformation/nullTransformer.hpp
@@ -0,0 +1,48 @@
+#ifndef THZ_IMAGE_HANDLING_NULLTRANSFORMER_HPP
+#define THZ_IMAGE_HANDLING_NULLTRANSFORMER_HPP
+
+#include "THzCommon/math/rectangle.hpp"
+#include "THzImage/common/iImageTransformer.hpp"
+
+namespace Terrahertz {
+
+/// @brief Enables default construction of IImageTransformers
+/// without the need for code in these classes handling default construction.
+///
+/// @tparam TPixelType The pixel type used by the transformer.
+template <typename TPixelType>
+class NullTransformer : public IImageTransformer<TPixelType>
+{
+public:
+    /// @brief Returns an instance of the NullTransformer.
+    ///
+    /// @return An instance of the NullTransformer.
+    static NullTransformer &instance() noexcept
+    {
+        static NullTransformer transformer{};
+        return transformer;
+    }
+
+    /// @copydoc IImageTransformer::dimensions
+    Rectangle dimensions() const noexcept override { return {}; }
+
+    /// @copydoc IImageTransformer::transform
+    bool transform(TPixelType &) noexcept override { return false; }
+
+    /// @copydoc IImageTransformer::skip
+    bool skip() noexcept override { return false; }
+
+    /// @copydoc IImageTransformer::reset
+    bool reset() noexcept override { return false; }
+
+    /// @copydoc IImageTransformer::nextImage
+    bool nextImage() noexcept override { return false; }
+
+private:
+    /// @brief Initializes a new instance of the NullTransformer.
+    NullTransformer() noexcept {}
+};
+
+} // namespace Terrahertz
+
+#endif // !THZ_IMAGE_HANDLING_NULLTRANSFORMER_HPP
---
+++ test/transformation/nullTransformer.cpp
@@ -0,0 +1,23 @@
+#include "THzImage/transformation/nullTransformer.hpp"
+
+#include <gtest/gtest.h>
+
+namespace Terrahertz::UnitTests {
+
+struct Transformation_NullTransformer : public testing::Test
+{};
+
+TEST_F(Transformation_NullTransformer, ReturnValuesAsExpected)
+{
+    auto &instance = NullTransformer<BGRAPixel>::instance();
+    EXPECT_EQ(instance.dimensions(), Rectangle{});
+    BGRAPixel const expectedPixel{0x34U, 0x11U, 0xBAU, 0xFEU};
+    BGRAPixel       pixel{expectedPixel};
+    EXPECT_FALSE(instance.transform(pixel));
+    EXPECT_EQ(pixel, expectedPixel);
+    EXPECT_FALSE(instance.skip());
+    EXPECT_FALSE(instance.reset());
+    EXPECT_FALSE(instance.nextImage());
+}
+
+} // namespace Terrahertz::UnitTests
